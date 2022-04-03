43fa1ea389feb20f53b7fdd7e6bf2342a8768274
Muhammad Kunjo <mkunjo121@gmail.com>
2022-04-03 10:50:39 -0700

    Page setup and intro to calculator's logic

---
+++ my-app/src/App.js
@@ -0,0 +1,54 @@
+import "./styles.css"
+import {useReducer} from "react"
+
+const ACTIONS = {
+  ADD_DIGIT: 'add delete',
+  CLEAR: 'clear',
+  CHOOSE_OPERATION: 'choose-operation',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+}
+
+function reducer(state, { type, payload }){
+  switch(type){
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state, 
+        operand: ${operand}${payload.digit}
+      }
+  }
+}
+
+function App() {     
+  const [{ operand, previousOperand, operation }, dispatch] = useReducer(reducer, {}) 
+  
+  dispatch({type: ACTIONS.ADD_DIGIT, payload: {digit: 1}})
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">{previousOperand} {operation}</div>
+          <div className="operand">{operand}</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
+    </div>
+  )
+}
+
+export default App;
